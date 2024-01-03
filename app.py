ddb4e16aa1bcd9b0cbeb81c636e59c64a0b49fe5
ruanovski <ruanvic21@gmail.com>
2024-01-03 14:24:43 -0300

    Adicionado o app.py como exemplo de REST API para manipular uma dados referente a desenvolvedores e suas habilidaes

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,jsonify,request
+import json
+
+app = Flask(__name__)
+
+desenvolvedores = [
+    {
+        "Nome": "Ruan Victor",
+        "Tecnologias": ["Python", "Django"]
+     },
+     {
+        "Nome": "Artur Bruno",
+        "Tecnologias": ["React", "Js", "java"]
+     }
+]
+
+@app.route("/dev/<int:id>/", methods = ["GET", "PUT", "DELETE"])
+def desenvolvedor(id):
+    if request.method == "GET":
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            response = {"status": "erro", "mensagem": "Desenvolvedor de ID {} não existe".format(id)}
+        return jsonify(response)
+    elif request.method == "PUT":
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == "DELETE":
+        desenvolvedores.pop(id)
+        return jsonify({"Status": "Sucesso", "Mensagem": "Registro excluido"})
+    
+@app.route("/dev/", methods=['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method == "POST":
+        dados = json.loads(request.data)
+        desenvolvedores.append(dados)
+        return jsonify({'status': 'Sucesso', 'Mensagem': 'Registro inserido'})
+    elif request.method == "GET":
+        return desenvolvedores
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
